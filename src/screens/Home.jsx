4978d79b63b122c3f29d3b9689a20ed6ded93d65
Agrim Khurana <agrimkh54321@gmail.com>
2020-07-26 04:16:24 +0530

    Purpose section done stepper to be added to event section

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,9 +3,10 @@ import { withStyles, AppBar, Toolbar, Typography, Button, Grid, Box } from "@mat
 import styles from '../assets/material/Home'
 import Star from '../assets/img/star.png'
 import Ribbon from '../assets/img/Ribbon.png'
-
+import HorizontalLinearStepper from './stepper'
 const Home = (props) => {
   const { classes } = props;
+  const stepper= HorizontalLinearStepper()
   return (
     <Fragment>
       {/* Header Element */}
@@ -35,16 +36,32 @@ const Home = (props) => {
       {/* Purpose Box */}
       <Grid item className={classes.LightBG}>
         <Typography variant='h2' align='center'>Purpose</Typography>
-        <Box width="45%">
-          <Typography align='center' variant='body1' className={classes.TextMargin}>Sociostory aims to build a community to drive sustainable social change in india</Typography>
-          <Typography align='center' variant='body1'>Through our platform, we aim to highlight the stories of corporate changemakers, non-profit organizations, and individuals whose initiatives are aligned to the Sustainable Development Goals (SDGs) and have impacted lives beyond the lifecycle of their project interventions.</Typography>
-        </Box>
+        <Grid container spacing={4}>
+          <Grid item xs={false} sm={2}></Grid>
+          <Grid item xs={12} sm={4}>
+          <Typography align='left' variant='body1' className={classes.TextMargin}>Sociostory aims to build a community to drive sustainable social change in India</Typography>
+          <Typography align='left' variant='body1'>
+            Through our platform, we aim to highlight the stories 
+          of corporate changemakers, non-profit organizations, and individuals whose initiatives are aligned 
+          to the Sustainable Development Goals (SDGs) and have impacted lives beyond 
+          the lifecycle of their project interventions.</Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+           <div style={{backgroundColor: 'white' , height: '83%',marginTop: 80,borderRadius:20}}>
+
+           </div>
+          </Grid>
+          <Grid item xs={false} sm={2}></Grid>
+          </Grid>
+        {/* <Box width="45%">
+          
+        </Box> */}
       </Grid>
 
       {/* Joing Leaders */}
       <Grid container className={classes.DarkBG}>
         <Grid item xs={12}>
-          <Typography>Join the impact leaders</Typography>
+          <Typography  variant='h2' align='center'>Join the Impact Leaders</Typography>
         </Grid>
       </Grid>
 
